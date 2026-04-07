fd5849e5b0c5cdcd14ebb65eb4cd9b4e9335b95f
Shakya77 <bijanshakya145@gmail.com>
2026-04-07 13:21:46 +0545

    few chagnes

---
+++ frontend/app/form/page.jsx
@@ -0,0 +1,5 @@
+import DynamicForm from "@/components/form/DynamicForm";
+
+export default function page() {
+  return <DynamicForm />;
+}
--- frontend/components/ecommerce/productDetails/ProductDetail.jsx
+++ frontend/components/ecommerce/productDetails/ProductDetail.jsx
@@ -1,7 +1,19 @@
 "use client";
 
-import { Carousel, Col, Divider, Image, List, Row, Typography } from "antd";
+import {
+  Button,
+  Carousel,
+  Col,
+  Divider,
+  Flex,
+  Image,
+  InputNumber,
+  List,
+  Row,
+  Typography,
+} from "antd";
 import { useRef, useState } from "react";
+import CardComponent from "../CardComponent";
 
 const { Title, Text } = Typography;
 
@@ -30,59 +42,91 @@ export default function ProductDetail() {
   const carouselRef = useRef(null);
   const [current, setCurrent] = useState(0);
 
+  const onChange = (value) => {
+    console.log(value);
+  };
+
   return (
-    <Row gutter={[16, 16]} justify="start" align="middle">
-      <Col span={12}>
-        <Carousel
-          ref={carouselRef}
-          afterChange={(index) => setCurrent(index)}
-          infinite={false}
-          arrows
-        >
-          {images.map((img, idx) => (
-            <div key={idx}>
+    <div className="container mx-auto p-4">
+      <Row gutter={32} justify="center" align="top">
+        <Col xs={24} sm={24} md={10} lg={6} style={{ textAlign: "center" }}>
+          <Carousel
+            ref={carouselRef}
+            afterChange={(index) => setCurrent(index)}
+            infinite={false}
+            arrows
+          >
+            {images.map((img, idx) => (
               <Image
                 src={img}
                 alt={`Product ${idx}`}
                 preview={false}
-                style={{ width: "100%", margin: "0 auto" }}
+                width={"fit-content"}
               />
-            </div>
-          ))}
-        </Carousel>
+            ))}
+          </Carousel>
 
-        <Row gutter={8} style={{ marginTop: 16, justifyContent: "center" }}>
-          {images.map((img, idx) => (
-            <Col key={idx}>
-              <Image
-                src={img}
-                preview={false}
-                width={60}
-                style={{
-                  border:
-                    current === idx ? "2px solid #1890ff" : "1px solid #d9d9d9",
-                  cursor: "pointer",
-                }}
-                onClick={() => {
-                  carouselRef.current.goTo(idx);
-                }}
-              />
-            </Col>
-          ))}
-        </Row>
-      </Col>
+          <Row gutter={8} style={{ marginTop: 16, justifyContent: "center" }}>
+            {images.map((img, idx) => (
+              <Col key={idx}>
+                <Image
+                  src={img}
+                  preview={true}
+                  width={60}
+                  style={{
+                    border:
+                      current === idx
+                        ? "2px solid #1890ff"
+                        : "1px solid #d9d9d9",
+                    cursor: "pointer",
+                  }}
+                  onClick={() => {
+                    carouselRef.current.goTo(idx);
+                  }}
+                />
+              </Col>
+            ))}
+          </Row>
+        </Col>
 
-      <Col span={12}>
-        <Title level={3}>
-          MSI MAG 255XF 300Hz 24.5 Inch FHD Gaming Monitor
-        </Title>
-        <Divider></Divider>
-        {data.map((item, index) => (
-          <li key={index}>
-            <Text style={{ display: "block", marginBottom: 8 }}>{item}</Text>
-          </li>
+        <Col xs={24} sm={24} md={14} lg={12}>
+          <Title level={3}>
+            MSI MAG 255XF 300Hz 24.5 Inch FHD Gaming Monitor
+          </Title>
+          <Divider></Divider>
+
+          <Title level={4}>Key Features :</Title>
+
+          {data.map((item, index) => (
+            <Text key={index} style={{ display: "block", marginBottom: 8 }}>
+              * {item}
+            </Text>
+          ))}
+
+          <Divider></Divider>
+
+          <Flex gap="medium">
+            <InputNumber
+              min={1}
+              max={10}
+              defaultValue={1}
+              onChange={onChange}
+            />
+
+            <Button type="primary" size="large">
+              Buy Now
+            </Button>
+          </Flex>
+        </Col>
+      </Row>
+
+      <Row gutter={[0, 32]} justify="start" align="top">
+        {Array.from({ length: 6 }).map((_, index) => (
+          <Col key={index} xs={24} sm={12} md={8} lg={6}>
+            <CardComponent id={index} />
+          </Col>
         ))}
-      </Col>
-    </Row>
+      </Row>
+    </div>
   );
 }
